f71618bf2b49f78b401e7bf6b81008eba5649d13
Goglow <waggle.glow@gmail.com>
2020-01-18 08:40:08 -0400

    Add Operation.swift and update different files

--- CountOnMe-OC/CountOnMe-OC/Controller/ViewController.swift
+++ CountOnMe-OC/CountOnMe-OC/Controller/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet var numberButtons: [UIButton]!
+    
+    let operation = Operation()
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
